cf94722e860571681a910eaaa2fc675cb461c3f5
dongfeng <sundongfeng@yazuo.com>
2018-07-01 01:28:44 +0800

    spring boot 定时任务

---
+++ src/main/java/com/kd/shequ/schedul/ScheduleTask.java
@@ -0,0 +1,21 @@
+package com.kd.shequ.schedul;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 定时任务
+ *
+ * @author sunny
+ * @create 2018/6/28 18:24
+ **/
+@Component
+public class ScheduleTask {
+
+    @Scheduled(fixedRate=5000)
+    public void currentTime(){
+        System.out.println("当前时间："+ LocalDateTime.now().toString());
+    }
+}
